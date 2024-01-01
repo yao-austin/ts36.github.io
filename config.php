6b3c5cd53bd04ac9afdda2cb2f9fc5f5d0a31afb
yao-austin <ge062851@gmail.com>
2024-01-02 00:32:42 +0800

    added new files

--- config.php
+++ config.php
@@ -1,4 +1,19 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','','food_order_system_db') or die('connection failed');
+// Connection details for MS SQL Server
+$serverName = "haha888.database.windows.net"; // Replace with your SQL Server's name or IP address
+$connectionOptions = array(
+    "Database" => "haha888", // Replace with your database name
+    "Uid" => "yaoyao", // Replace with your SQL Server username
+    "PWD" => "Qq0989260287" // Replace with your SQL Server password
+);
+
+// Establish a connection to MS SQL Server
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+// Check the connection
+if (!$conn) {
+    die(print_r(sqlsrv_errors(), true));
+}
+
 ?>
